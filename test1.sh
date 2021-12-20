977454328031d462164b05a572cee6f443d06c3d
木四水 <78152109+ZMuSiShui@users.noreply.github.com>
2021-12-21 02:30:35 +0800

    Update test1.sh

--- test1.sh
+++ test1.sh
@@ -1,2 +1,3 @@
 sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 sed -i '$a src-git jerryk https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
