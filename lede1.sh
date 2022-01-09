bb3b479f45a5b816aed9774c7149440d20d44d0a
18816230015 <70273048+18816230015@users.noreply.github.com>
2022-01-09 12:59:16 +0800

    Update lede1.sh

--- lede1.sh
+++ lede1.sh
@@ -1,3 +1,4 @@
 sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 sed -i '$a src-git jerryk https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
+sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
